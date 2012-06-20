49f3e60114b0aaddce0ad2f4e3c1523f773704e1
Patrik Hedman <patrik.hedman@gmail.com>
2012-06-20 19:12:10 +0200

    did some restructuring

--- test/parser_test.js
+++ test/parser_test.js
@@ -4,8 +4,11 @@ require("../lib/espresso");
 var Parser = require('../lib/grammars/parser.ometajs').Parser
 	, _      = require('../lib/nodes');
 
-describe("Dummy Test", function () {
-	it ("works", function () {
-		Parser.parse('foobar').should.eql(_.File([['foobar']]));
-	});
+describe("Parser", function () {
+  var parser;
+  beforeEach(function () {
+    parser = Parser.clone();
+  });
+  
+  
 });
