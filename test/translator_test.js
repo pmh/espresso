8757d69a1b55f505b05664a29a0ae95aaa35a40e
Patrik Hedman <patrik.hedman@gmail.com>
2012-07-13 00:23:12 +0200

    rephrased test descriptions

--- test/translator_test.js
+++ test/translator_test.js
@@ -15,47 +15,47 @@ describe("Translator", function () {
 
   describe("Identifiers", function () {
 
-    it("should be able to translate identifiers", function() {
+    it("should translate identifiers", function() {
       compile('foo', "identifier").should.eql("foo");
     });
   });
 
   describe("Numbers", function () {
-    it("should be able to translate integers", function() {
+    it("should translate integers", function() {
       compile("2").should.eql("2");
     });
 
-    it("should be able to translate negative integers", function() {
+    it("should translate negative integers", function() {
       compile("-2").should.eql("-2");
     });
 
-    it("should be able to translate floating points", function() {
+    it("should translate floating points", function() {
       compile("2.345").should.eql("2.345");
     });
 
-    it("should be able to translate negative floating points", function() {
+    it("should translate negative floating points", function() {
       compile("-2.345").should.eql("-2.345");
     });
 
-    it("should be able to translate underscore separated numbers", function() {
+    it("should translate underscore separated numbers", function() {
       compile("2_000_000").should.eql("2000000");
     });
   });
 
   describe("Strings", function() {
-    it ("should be able to translate regular strings", function () {
+    it ("should translate regular strings", function () {
       compile('"foo bar"').should.eql('"foo bar"');
     });
     
-    it ("can translate escaped strings", function() {
+    it ("should translate escaped strings", function() {
       compile('"foo \\"bar\\""').should.eql('"foo \\"bar\\""');
     });
 
-    it("can translate multi-line strings", function() {
+    it("should translate multi-line strings", function() {
       compile('"foo\nbar\nbaz"').should.eql('"foo\\nbar\\nbaz"')
     });
     
-    it ("can translate interpolated strings", function () {
+    it ("should translate interpolated strings", function () {
       compile('"foo #{1} bar #{"baz"}"', 'expr').should.eql('["foo ", 1, " bar ", "baz"].join("")');
     });
   });
