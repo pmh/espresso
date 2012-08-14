e3fae7cc5c73f7e7d134c85d127467ac7718f8ce
Patrik Hedman <patrik.hedman@gmail.com>
2012-08-14 12:56:57 +0200

    created the espresso binary

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var Espresso = require('./lib/espresso');
-
-Espresso.parse(require("fs").readFileSync("./examples/HelloWorld.es", "utf8")).print_tree();
-
-console.log('\n--------------------\n');
-
-eval(Espresso.compileFile("./examples/HelloWorld.es"));
+module.exports = require('./lib/espresso');
